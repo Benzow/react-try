d07639b5e62166a7f26dfc34b64eb7b1da7642a8
Benzow <benzow2006@gmail.com>
2021-02-25 23:28:01 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./style.css";
-
+let num1=8
+let num2 = 9
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+{`${num1
+}
+${num2}`}
     </div>
+    
   );
 }
